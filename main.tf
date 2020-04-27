eb44321c778567d027202b3cbfbd04c031d90e1c
raviatlantis <k.raviratna@gmail.com>
2020-04-26 21:51:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  access_key = "AWS_SECRET_KEY"
-  secret_key = "AWS_ACCESS_KEY"
-  region     = "us-east-1"
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+ region     = "us-east-1"
 }
